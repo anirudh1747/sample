7c639bd98c5f14e59f78f26a13be5cdcad3833d5
anirudh1747 <61910937+anirudh1747@users.noreply.github.com>
2021-12-04 05:41:12 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,77 @@
+let telugu = [
+    {
+      name: "Single",
+      artist: "Jordan Schor",
+      img: "1",
+      src: "Single"
+    },
+    {
+      name: "Amigo",
+      artist: "Audio Library",
+      img: "1",
+      src: "Amigo"
+    },
+    {
+      name: "Veedhikoka Jaathi",
+      artist: "Beauz & Jvna",
+      img: "1",
+      src: "Veedhikoka Jaathi"
+    },
+    {
+      name: "Seatu Siragadha",
+      artist: "Mike Archangelo",
+      img: "1",
+      src: "Seatu Siragadha"
+    },
+    {
+      name: "Telavaarutunte",
+      artist: "Jim Yosef x Roy",
+      img: "1",
+      src: "Telavaarutunte"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "1",
+      src: "example"
+    },
+];
+
+let english = [
+    {
+      name: "test ",
+      artist: "test artist",
+      img: "faded",
+      src: "faded"
+    },
+    {
+      name: "Faded - Alan Walker",
+      artist: "Alan Walker",
+      img: "faded",
+      src: "faded"
+    },
+    {
+      name: "letme – Ikson",
+      artist: "DJ Snake",
+      img: "letme",
+      src: "letme"
+    },
+    {
+      name: "memories",
+      artist: "Maroon 5",
+      img: "memories",
+      src: "memories"
+    },
+    {
+      name: "takitaki",
+      artist: "DJ Snake",
+      img: "takitaki",
+      src: "takitaki"
+    },
+    {
+      name: "shawn",
+      artist: "Shawn",
+      img: "shawn",
+      src: "shawn"
+    },
+];
